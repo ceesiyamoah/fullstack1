152812e7c9826d83292f451ede15b80572714585
Cyril <ccyamoah@gmail.com>
2024-05-08 11:25:09 +0000

    products api

---
+++ app/api/products/route.tsx
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import schema from './schema';
+import prisma from '@/prisma/client';
+
+export async function GET(req: NextRequest) {
+	const products = await prisma.product.findMany({});
+	return NextResponse.json(products);
+}
+
+export async function POST(req: NextRequest) {
+	const body = await req.json();
+	const { success, error, data } = schema.safeParse(body);
+	if (!success) return NextResponse.json({ error: error.errors }, { status: 400 });
+	const product = await prisma.product.create({
+		data,
+	});
+
+	return NextResponse.json(product);
+}
--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 import schema from '../schema';
+import prisma from '@/prisma/client';
 
 interface Props {
 	params: {
-		id: number;
+		id: string;
 	};
 }
 
-export function GET(request: NextRequest, { params: { id } }: Props) {
-	if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
-	return NextResponse.json({ id });
+export async function GET(request: NextRequest, { params: { id } }: Props) {
+	const user = await prisma.user.findUnique({ where: { id: +id } });
+	if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+	return NextResponse.json(user);
 }
 
 export async function PATCH(request: NextRequest, { params: { id } }: Props) {
@@ -17,12 +20,26 @@ export async function PATCH(request: NextRequest, { params: { id } }: Props) {
 	const validation = schema.safeParse(body);
 	if (!validation.success) return NextResponse.json({ error: validation.error.errors }, { status: 400 });
 
-	if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+	const user = await prisma.user.findUnique({ where: { id: +id } });
 
-	return NextResponse.json({ id: 1, name: 'Cyril' });
+	if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+	const updatedUser = await prisma.user.update({
+		where: { id: user.id },
+		data: {
+			name: body.name,
+			email: body.email,
+		},
+	});
+
+	return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
-	if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+	const user = await prisma.user.findUnique({ where: { id: +id } });
+
+	if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+	await prisma.user.delete({ where: { id: user.id } });
+	// if (id > 10) return NextResponse.json({ error: 'User not found' }, { status: 404 });
 	return NextResponse.json({}, { status: 202 });
 }
